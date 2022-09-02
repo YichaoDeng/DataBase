ce995f8de6691561f43e4bf7ef7af791289a06c2
YichaoDeng <Yichao.Deng@icloud.com>
2022-09-02 23:23:12 +0800

    update: finish mysql simple class test

--- MySqlDB.py
+++ MySqlDB.py
@@ -1,113 +1,228 @@
+import subprocess
+import time
+from pprint import pprint
+
 import pymysql
 import traceback
 from loguru import logger
 
+USER = "root"
+PASSWORD = "Zz123456"
+HOST = "127.0.0.1"
+DATABASE = "test"
+PORT = 3306
 
-class MySQL(object):
+mysql_config = {
+    'host': HOST,
+    'user': USER,
+    'password': PASSWORD,
+    'database': DATABASE,
+    'port': PORT,
+    'charset': 'utf8mb4',
+    'autocommit': True
+}
+
+logger.add(sink='log.txt', format="{time}, {level}, {message}", level="INFO")
+
+
+def mysql_log_wrapper(func):
+    """
+    mysql 日志装饰器
+    :param func:
+    :return:
+    """
+
+    def inner(*args, **kwargs):
+        timestamp = int(time.time() * 1000)
+        ret = func(*args, **kwargs)  # noqa
+        user = args[0].basic_config.get('user')
+        sql = args[1]
+        logger.info(f"{timestamp}, {user}, {sql}, {ret}")
+        return ret
+
+    return inner
+
+
+class Singleton(type):
+    """
+    单例模式
+    """
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class MySQL(metaclass=Singleton):
+    """
+    MySQL 工具类
+    """
 
     def __init__(self,
-                 host: str = '127.0.0.1',
-                 port: int = 3306,
-                 user: str = 'root',
-                 password: str = None,
-                 database: str = 'mysql',
-                 charset: str = 'utf8mb4',
-                 autocommit: bool = True,
-                 *args, **kwargs
+                 basic_config: dict = None,
+                 **kwargs
                  ) -> None:
-        self.host = host
-        self.port = port
-        self.user = user
-        self.password = password
-        self.database = database
-        self.charset = charset
-        self.autocommit = autocommit
-        self.args = args
+        """
+        初始化 mysql 连接配置
+        :param basic_config:
+        :param kwargs:
+        """
+        self.basic_config = basic_config
         self.kwargs = kwargs
+        self.cursor_type = kwargs.pop('cursor_type') if kwargs.get('cursor_type') else 'set'
         self.__connect()
         self.__create_cursor()
 
     def __connect(self):
+        """
+        连接 mysql
+        :return:
+        """
         self.__conn = pymysql.connect(
-            host=self.host,
-            port=self.port,
-            user=self.user,
-            password=self.password,
-            database=self.database,
-            charset=self.charset,
-            autocommit=self.autocommit,
-            *self.args, **self.kwargs
+            **self.basic_config
         )
 
     def __create_cursor(self):
-        self.cursor = self.__conn.cursor()
+        """
+        创建游标
+        :return:
+        """
+        self.cursor = self.__conn.cursor() if \
+            self.cursor_type == 'set' else \
+            self.__conn.cursor(
+                pymysql.cursors.DictCursor)  # noqa
 
     @property
     def status(self):
+        """
+        获取 db 连接状态
+        :return:
+        """
         return self.__conn.open
 
-    # @property
-    # def cursor(self, **kwargs):
-    #     return self.cursor
-
-    # def __connect(self):  # 加下划线就叫私有方法，方法或者变量前面加，就叫私有方法。只能自己调用在这个类里面
-    #
-    #     try:
-    #         self.__conn = pymysql.connect(**self.mysql_info)  #
-    #     except Exception:
-    #         logger.error("mysql连接失败！mysql的连接信息是{},错误信息,{}",  # 日志
-    #                      self.mysql_info, traceback.format_exc())  # 错误信息
-    #         quit()
-    #
-    # def __create_cursor(self):  # 私有
-    #     self.cursor = self.__conn.cursor() if self.cur_type == 1 else self.__conn.cursor(pymysql.cursors.DictCursor)
-
+    @mysql_log_wrapper
     def execute_sql(self, sql):
+        """
+        执行 sql 语句
+        :param sql:
+        :return:
+        """
         try:
-            print(self.cursor.execute(sql))
-        except Exception:
-            logger.warning("sql语句执行出错,sql是:{},报错信息:{}", sql, traceback.format_exc())
+            self.cursor.execute(sql)
+        except Exception:  # noqa
+            print(f"sql语句执行出错,sql是:{sql},报错信息:{traceback.format_exc()}")
+            return False
         else:
             return True
 
     def fetchone(self, sql):
+        """
+        获取一条数据
+        :param sql:
+        :return:
+        """
         sql_tag = self.execute_sql(sql)
         if sql_tag:
             result = self.cursor.fetchone()
-            logger.debug("本次sql查询的结果是:{}", result)
             return result
 
     def fetchall(self, sql):
+        """
+        获取所有数据
+        :param sql:
+        :return:
+        """
         sql_tag = self.execute_sql(sql)
         if sql_tag:
             result = self.cursor.fetchall()
-            logger.debug("本次sql查询的结果是:{}", result)
             return result
 
     def __close(self):
-        if hasattr(self, "conn"):  # 判断当前这个对象里面有没有conn这个属性
+        """
+        关闭游标连接
+        :return:
+        """
+        if hasattr(self, "conn"):
             self.__conn.close()
             self.cursor.close()
-    # update insert delete  create drop
+
+
+class ServerTools:
+    """
+    服务器工具类
+    """
+
+    @classmethod
+    def run_command(cls, command, timeout=10):
+        """
+        服务器 指令执行器
+        :param command: 执行
+        :param timeout: 超时时间
+        :return:
+        """
+        popen = subprocess.Popen(
+            command,
+            encoding="utf-8",
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=True
+        )
+        try:
+            result, error = popen.communicate(timeout=timeout)
+        except:  # noqa
+            result = ''
+            error = '获取返回值超时, 请检查是否为阻塞型任务'
+
+        return {
+            'code': popen.returncode,
+            'msg': error or "success",
+            'result': result,
+        }
+
+    @classmethod
+    def run_port_monitor(cls, port, reset=False):
+        """
+        运行 端口监控统计
+        :param port: 端口号
+        :param reset: 是否重置输入输出统计
+        :return:
+        """
+        if reset:
+            cls.run_command(f'iptables -Z INPUT')
+            cls.run_command(f'iptables -Z OUTPUT')
+
+        cls.run_command(f'iptables -A INPUT -p tcp --dport {port}')  # noqa
+        cls.run_command(f'iptables -A OUTPUT -p tcp --sport {port}')
+
+    @classmethod
+    def get_port_info(cls, port, reset=False):
+        """
+        获取 端口流量统计信息
+        :param port: 端口号
+        :param reset: 是否重置输入输出统计
+        :return:
+        """
+        cls.run_port_monitor(port=port, reset=reset)
+        return cls.run_command(f'iptables -L -v -n -x')
 
 
 if __name__ == '__main__':
-    # mysql_info = {
-    #     "host": "110.40.129.51",
-    #     "user": "jxz",
-    #     "passwd": "123456",
-    #     "db": "jxz",
-    #     "port": 3306,
-    #     "charset": "utf8",
-    #     "autocommit": True
-    # }
-    # mysql = MySQL(mysql_info)
-    # mysql.execute_sql("select * from user_ztt")
-    # for line in mysql.cursor:
-    #     print(line)
-    # mysql.fetchall("select * from user_ztt limit 5;")
-    # mysql.fetchone("select * from user_ztt where id =3")
-    mysql = MySQL(password='Zz123456', database='test')
-    print(mysql.status)
-    res = mysql.fetchone(sql='select * from test_table limit 10')
-    print(res)
+    mysql = MySQL(basic_config=mysql_config, cursor_type='dict')
+    ret_0 = mysql.fetchone(sql='xxxxxxx')  # noqa
+    ret_1 = mysql.fetchone(sql='select * from test_table limit 10')  # noqa
+    ret_2 = mysql.fetchall(sql='select * from test_table limit 10')  # noqa
+    print(f"""
+    ret_0:
+        {ret_0}
+    ret_1:
+        {ret_1}
+    ret_2:
+        {ret_2}
+    """)
+
+    server = ServerTools()
+    ret = server.get_port_info(port=3306)
+    pprint(ret)
